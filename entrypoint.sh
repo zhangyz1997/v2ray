f63feb8f4f71d6b347871ecafe1defbfb2c65944
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-03-12 22:01:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,8 @@
+cd /go/bin/v2ray-linux-64
 
+if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+  echo -e "$CERT_PEM" > cert.pem
+  echo -e "$KEY_PEM"  > key.pem
+  echo -e "$CONFIG_JSON" > config.json
+fi
+./v2ray -c config.json
