22fdde6f8060207d0c47fa704f3b0daa68e537e4
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-03-13 19:35:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 cd /v2ray/v2ray-v$VER-linux-64
-
-if [ "$V2RAY_CRT" != "$V2RAY_KEY" ]; then
-  echo -e "$V2RAY_CRT" > v2ray.crt
-  echo -e "$V2RAY_KEY" > v2ray.key
-  echo -e "$CONFIG_JSON" > config.json
-fi
+echo -e "$V2RAY_CRT" > v2ray.crt
+echo -e "$V2RAY_KEY" > v2ray.key
+echo -e "$CONFIG_JSON" > config.json
 ./v2ray
